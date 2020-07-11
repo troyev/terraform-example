ef4eb8a11459de96c4a2c80e92fc12a922aa06c7
Troy Velius <troyvelius@gmail.com>
2020-07-11 13:45:16 -0400

    initial commit, switching to micro instances

---
+++ eks-cluster.tf
@@ -0,0 +1,33 @@
+module "eks" {
+  source       = "terraform-aws-modules/eks/aws"
+  wait_for_cluster_interpreter = ["C:/Program Files/Git/bin/sh.exe", "-c"]
+  wait_for_cluster_cmd         = "for i in `seq 1 60`; do wget --no-check-certificate -O - -q $ENDPOINT/healthz >/dev/null && exit 0 || true; sleep 5; done; echo TIMEOUT && exit 1"
+  cluster_name = local.cluster_name
+  subnets      = module.vpc.private_subnets
+
+  tags = {
+    Environment = "training"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+  }
+
+  vpc_id = module.vpc.vpc_id
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.micro"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    }
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
